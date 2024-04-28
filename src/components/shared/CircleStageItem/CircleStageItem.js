407d92efe9997f4cfa59abac30e22e43638e1fd4
nazartsap21 <nazar.tsap.ua@gmail.com>
2024-04-28 21:50:03 +0300

    fix dots

--- src/components/shared/CircleStageItem/CircleStageItem.js
+++ src/components/shared/CircleStageItem/CircleStageItem.js
@@ -7,17 +7,19 @@ const CircleStageItem = ({step}) => {
         <div className='circle-stage-item'>
 
             <div className={'circle-container'} >
-                <h3>Привіт</h3>
+                <h4>Привіт</h4>
                 <div className='circle' style={{backgroundColor: step >= 1 ? 'black' : 'white'}}></div>
                 {/*<div className='line' style={{backgroundColor: step >= 2 ? 'black' : 'white'}}></div>*/}
             </div>
             <div className='circle-container' style={{bottom: 0, left: '54px'}}>
+                <div style={{height: "19px"}}></div>
                 <div className='circle' style={{backgroundColor: step >= 2 ? 'black' : 'white'}}></div>
-                <h3>Зареєструйся</h3>
+                <h4>Зареєструйся</h4>
+                {/*<h3>Зареєструйся</h3>*/}
                 {/*<div className='line' style={{backgroundColor: step >= 3 ? 'black' : 'white'}}></div>*/}
             </div>
             <div className='circle-container' style={{top: 0, right: 0}}>
-                <h3>Користуйся</h3>
+                <h4>Користуйся</h4>
                 <div className='circle' style={{backgroundColor: step >= 3 ? 'black' : 'white'}}></div>
             </div>
         </div>
